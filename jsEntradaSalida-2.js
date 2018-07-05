1527f4cfa50e5059569de9f607c5f0a18dcd435f
Francomartin <40859124+Francomartin@users.noreply.github.com>
2018-07-05 11:35:57 -0300

    Primera clase

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,11 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+//esto es un comentario //var person = prompt("Please enter your name", "Harry Potter");
+var nombre;
+nombre="franco";
+
+nombre=prompt("ingrese su sueldo");
+
+}

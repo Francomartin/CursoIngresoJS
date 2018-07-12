3028c2cb7cf5b64755cb9a707dd090cca50cba1e
Francomartin <40859124+Francomartin@users.noreply.github.com>
2018-07-12 12:22:13 -0300

    Add files via upload

---
+++ jsInstruccionIF (9).js
@@ -0,0 +1,15 @@
+function mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 
+	var random;
+
+
+	random=Math.floor(Math.random(FormIngreso) * 10) + 1;  // returns a random integer from 1 to 10
+	console.log(random);
+
+	alert(random);
+
+
+
+
+}//FIN DE LA FUNCIÓN
